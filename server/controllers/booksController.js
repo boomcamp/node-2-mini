9e4d3263d9afa9e87d70e1fe5a1475872fc38627
Martha Dansyle Marbella <martha.marbella@boom.camp>
2019-12-30 16:14:29 +0800

    Node-2-mini Done

---
+++ server/controllers/booksController.js
@@ -0,0 +1,37 @@
+let books = [];
+let id = 0;
+
+module.exports = {
+   read: (req, res) => {
+      res.status(200).json(books);
+   },
+   create: (req, res) => {
+      const { title, author } = req.body;
+      let book = {
+         id,
+         title,
+         author
+      };
+      books.push(book);
+      id++;
+      res.status(200).send(books);
+   },
+   update: (req, res) => {
+      const { title, author } = req.body;
+      const { bookId } = req.params;
+
+      const bookIndex = books.findIndex(book => book.id === parseInt(bookId));
+      books[bookIndex] = { title, author };
+
+      res.status(200).json(books);
+   },
+   delete: (req, res) => {
+      const { bookId } = req.params;
+
+      const bookIndex = books.findIndex(book => book.id === parseInt(bookId));
+
+      books.splice(bookIndex, 1);
+
+      res.status(200).json(books);
+   }
+};
