5a78d785019ca10913c65a9aca448bdfed454c91
marcial norte <marcial.norte@boom.camp>
2020-01-03 10:25:27 +0800

    Step 4 done

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`);
+});
