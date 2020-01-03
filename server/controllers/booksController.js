cf7fb0adbdf3af59fcf5acef0ae73de1812a61be
Christian Ray Frecia <christian.frecia@boom.camp>
2020-01-03 13:52:12 +0800

    done

---
+++ server/controllers/booksController.js
@@ -0,0 +1,44 @@
+let books = [];
+let id = 0;
+
+module.exports = {
+  read: (req, res) => {
+    res.status(200).send(books)
+  },
+  create: (req,res) => {
+    const {title, author} = req.body;
+    let book = {
+      id,
+      title,
+      author,
+    };
+    books.push(book);
+    id++;
+    res.status(200).send(books);
+  },
+  update: (req, res) => {
+    const bookToUpdate = books.find(book => Number(req.params.id) === book.id);
+    if (bookToUpdate) {
+      const { title, author } = req.body;
+
+      Object.assign(bookToUpdate, {
+        ...(title && { title }), // conditionally applies if title is defined
+        ...(author && { author }), // conditionally applies if author is defined
+      });
+      return res.status(200).send(books);
+    }
+    return res.status(404).send({ error: 'Could not find specified book.' });
+  },
+  delete: (req, res) => {
+    const { id } = req.params;
+    const bookIndex = books.findIndex(
+      book => Number(req.params.id) === book.id
+    );
+    if (id > -1) {
+      books.splice(bookIndex, 1);
+      return res.status(200).send(books);
+    }
+
+    return res.status(404).send({ error: 'Could not find specified book.' });
+  },
+};
