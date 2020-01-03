95df4422b1e5e40099abba4c3887d3284a272312
jerommel-astillero <55724630+jerommel@users.noreply.github.com>
2020-01-03 11:57:47 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.post("/api/books", bc.create);
 app.put("/api/books/:id", bc.update);
 app.delete("/api/books/:id", bc.delete);
 
-const port = 3001;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
