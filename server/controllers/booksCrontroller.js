9f7a5059146b215056f7b3cc0b2347365b545066
marcial norte <marcial.norte@boom.camp>
2020-01-03 11:32:16 +0800

    Step 7 done

--- server/controllers/booksCrontroller.js
+++ server/controllers/booksCrontroller.js
@@ -4,5 +4,16 @@ let id = 0;
 module.exports = {
 	read: (req, res) => {
 		res.status(200).send(books);
+	},
+	create: (req, res) => {
+		const { title, author } = req.body;
+		let book = {
+			id,
+			title,
+			author
+		};
+		books.push(book);
+		id++;
+		res.status(200).send(books);
 	}
 };
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const bc = require("./controllers/booksController.js");
+const bc = require("./controllers/booksCrontroller");
 const app = express();
 
 app.use(express.json());
 
 app.get("/api/books", bc.read);
+app.post("/api/books", bc.create);
 
 const PORT = 3001;
 
