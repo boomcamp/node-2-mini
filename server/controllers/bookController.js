4dd81ebf86419898bd252802b2706689eec68c25
rapraquion <rapraquion@gmail.com>
2020-01-03 09:58:05 -0500

    Step 8 done.

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -14,7 +14,19 @@ module.exports = {
             author
         };
         books.push(book);
-        id++
-        res.status(200).send(books)
+        id++;
+        res.status(200).send(books);
+    },
+    update: (req, res) => {
+        const bookUpdate = books.find(book => Number(req.params.id) === book.id);
+        if (bookUpdate) {
+            const { title, author } = req.body;
+            Object.assign(bookUpdate, {
+                ...(title && { title }),
+                ...(author && { author }),
+            });
+            return res.status(200).send(books);
+        }
+        return res.status(400).send({ error: 'Could not book.' })
     }
-}
+};
--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ app.use(express.json());
 
 app.get('/api/books', bc.read);
 
-app.post('/api/books/', bc.create);
+app.post('/api/books', bc.create);
+
+app.put('/api/books/:id', bc.update)
 
 const port = 3000;
 
