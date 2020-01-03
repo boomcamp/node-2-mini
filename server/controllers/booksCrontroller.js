7d6277636e11340102777af69f5812811a78fa91
marcial norte <marcial.norte@boom.camp>
2020-01-03 11:32:39 +0800

    Step 8 done

--- server/controllers/booksCrontroller.js
+++ server/controllers/booksCrontroller.js
@@ -15,5 +15,14 @@ module.exports = {
 		books.push(book);
 		id++;
 		res.status(200).send(books);
+	},
+	update: function(req, res) {
+		const { title, author } = req.body;
+		const { bookId } = req.params;
+
+		const bookIndex = books.findIndex(book => book.id === parseInt(bookId));
+		books[bookIndex] = { title, author };
+
+		res.status(200).json(books);
 	}
 };
--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 
 app.get("/api/books", bc.read);
 app.post("/api/books", bc.create);
+app.put("/api/books/:bookId", bc.update);
 
 const PORT = 3001;
 
